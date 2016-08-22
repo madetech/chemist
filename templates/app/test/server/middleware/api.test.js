d1d0490639336903fdc001e9ccdede8bf9b0d269
Richard Foster <richard@madetech.co.uk>
2016-08-22 09:47:56 +0100

    Remove middleware specs from generated app
    We'll test these directly in the framework soon, not nice generating
    apps with failing test suites

--- templates/app/test/server/middleware/api.test.js
+++
@@ -1,8 +0,0 @@
-describe('Middleware', function () {
-  describe('API Proxy Middleware', function () {
-    it('should proxy requests to the API', async function () {
-      const response = await fetch(`${APP_HOST}/api/ping`, { method: 'HEAD' })
-      expect(response.status).to.eq(200)
-    })
-  })
-})
