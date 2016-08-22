28c17640f85141bbc80c3c8e24aad354fff214e5
Richard Foster <richard@madetech.co.uk>
2016-08-22 09:01:03 +0100

    Remove unused less loader

--- src/config/webpack/development.config.js
+++ src/config/webpack/development.config.js
@@ -45,10 +45,6 @@ module.exports = {
         loader: 'json-loader'
       },
       {
-        test: /\.less$/,
-        loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!less?outputStyle=expanded&sourceMap'
-      },
-      {
         test: /\.scss$/,
         exclude: /shared\/vendor/,
         loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap'
