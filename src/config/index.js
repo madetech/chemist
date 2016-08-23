76b8e523e20126af20ab0641feb3332c47444a9a
Richard Foster <richard@madetech.co.uk>
2016-08-23 12:11:01 +0100

    Allow applications to override framework config

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,7 @@
+import path from 'path'
 import createConfig from './createConfig'
 
-export default createConfig([__dirname])
+export default createConfig([
+  __dirname,
+  path.join(process.cwd(), './config')
+])
