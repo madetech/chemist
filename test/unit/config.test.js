ce968d9b070f196bbaaf4d3089bd194b0fdd7a79
Richard Foster <richard@madetech.co.uk>
2016-08-23 23:15:09 +0100

    Namespace config tests

--- test/unit/config.test.js
+++ test/unit/config.test.js
@@ -3,18 +3,20 @@ import createConfig from '../../src/config/createConfig'
 
 const config = createConfig([path.join(__dirname, '../fixtures/config')])
 
-describe('createConfig', function () {
-  it('creates a function which returns config from the given directories', function () {
-    expect(config).to.be.a('function')
-  })
-})
-
-describe('config', function () {
-  it('returns the exports of the file you pass in', function () {
-    expect(config('test')).to.have.all.keys('a', 'b', 'c')
+describe('Configuration', function () {
+  describe('createConfig', function () {
+    it('creates a function which returns config from the given directories', function () {
+      expect(config).to.be.a('function')
+    })
   })
 
-  it('merges config for the current environment into the default config', function () {
-    expect(config('test')).to.deep.equal({ a: 1, b: 12, c: 3 })
+  describe('config', function () {
+    it('returns the exports of the file you pass in', function () {
+      expect(config('test')).to.have.all.keys('a', 'b', 'c')
+    })
+
+    it('merges config for the current environment into the default config', function () {
+      expect(config('test')).to.deep.equal({ a: 1, b: 12, c: 3 })
+    })
   })
 })
