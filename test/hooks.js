f7c992197854bcac0950df1aee43ff612e94119d
Richard Foster <richard@madetech.co.uk>
2016-08-28 11:31:09 +0100

    Revert "Temporarily remove test-app installation"
    This reverts commit 5f7cb8b8e8fb2bc5f62c9002f96be50d397a7b0f.

--- test/hooks.js
+++ test/hooks.js
@@ -1,12 +1,18 @@
 import { fork, spawn } from 'child_process'
 import path from 'path'
+import rimraf from 'rimraf'
 
 const TEST_APP_NAME = 'test-app'
 const cwd = path.join(__dirname, '../')
+const appCwd = path.join(cwd, TEST_APP_NAME)
+
+const install = cb =>
+  spawn('npm', ['install'], { cwd: appCwd, silent: true })
+    .on('close', () => cb())
 
 const scaffold = cb =>
   fork('dist/bin/cli', ['new', TEST_APP_NAME], { cwd, silent: true })
-    .on('close', () => cb())
+    .on('close', () => install(cb))
 
 const build = cb =>
   spawn('npm', ['run', 'build'])
@@ -16,3 +22,5 @@ before(function (done) {
   this.timeout(10000)
   build(done)
 })
+
+after(done => rimraf(path.join(cwd, TEST_APP_NAME), done))
