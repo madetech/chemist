fe5983bd43482b33841c29c4bf753209e3dea03c
Richard Foster <richard@madetech.co.uk>
2017-01-05 15:28:18 +0000

    Make asset dev server configurable in applications

--- src/bin/cli/commands/watch.js
+++ src/bin/cli/commands/watch.js
@@ -1,5 +1,3 @@
-import path from 'path'
-import fs from 'fs'
 import express from 'express'
 import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
@@ -7,37 +5,18 @@ import webpackHotMiddleware from 'webpack-hot-middleware'
 import config from '../../../config'
 import * as Logger from '../helpers/logger'
 
-const configPath = path.join(process.cwd(), './config')
-
-let appConfig = {}
-
-if (fs.existsSync(configPath)) {
-  appConfig = require(configPath)
-}
-
 export default function watch () {
   process.env.NODE_ENV = 'development'
   process.env.UV_THREADPOOL_SIZE = 100
+  const { app: appConfig } = config('index')
   const webpackConfig = config('webpack')
+  const serverConfig = config('webpack-dev-server')
 
   const compiler = webpack(webpackConfig)
-  const host = appConfig.host || 'localhost'
   const port = appConfig.webpackPort || 3001
   const app = express()
 
-  const serverOptions = {
-    contentBase: `http://${host}:${port}`,
-    quiet: true,
-    noInfo: true,
-    hot: true,
-    inline: true,
-    lazy: false,
-    publicPath: webpackConfig.output.publicPath,
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    stats: { colors: true }
-  }
-
-  app.use(webpackDevMiddleware(compiler, serverOptions))
+  app.use(webpackDevMiddleware(compiler, serverConfig))
   app.use(webpackHotMiddleware(compiler))
   app.listen(port, function (err) {
     if (err) {
